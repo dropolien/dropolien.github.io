e23e2948ecda5ef88d7a91025ec92e45b70406e8
lars <lg@goforlag.dk>
2020-03-03 14:33:04 +0100

    vh hack

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 var letterEl = document.getElementById("letter")
 
