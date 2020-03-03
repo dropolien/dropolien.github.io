cb5a40826e509fa06bc8d8d3e8660d24f349a2db
lars <lg@goforlag.dk>
2020-03-03 16:28:38 +0100

    refactor

--- script.js
+++ script.js
@@ -2,22 +2,32 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-var letterEl = document.getElementById("letter")
+
+var closeEl = document.querySelector(".letter__close")
+var letterEl = document.querySelector(".letter")
+
 
 function handleScroll(e) {
-  var scrollTop = window.scrollY
-  var letterEl = document.getElementById("letter")
-
-    if (scrollTop > 10) {
+    if (window.scrollY > 10) {
       letterEl.classList.add("letter--shown")
+      letterEl.classList.remove("js-letter-open")
     } else {
       // letterEl.classList.remove("letter--shown")
     }
 }
 
+
 window.addEventListener('scroll', handleScroll);
 
-window.addEventListener('click', function () {
-  var letterEl = document.getElementById("letter")
-  letterEl.classList.add("letter--shown")
+letterEl.addEventListener('click', function () {
+    letterEl.classList.add("letter--shown")  
+})
+
+
+
+closeEl.addEventListener('click', function (e) {  
+  console.log('close');
+  
+  letterEl.classList.toggle("letter--shown");
+  e.stopPropagation();
 })
