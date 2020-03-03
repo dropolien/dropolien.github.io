4db0d559fa9f31d4598bbca201255f8a73a38d9e
lars <lg@goforlag.dk>
2020-03-03 16:45:32 +0100

    ny vh

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-let vh = window.innerHeight * 0.01;
+var vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-
-var closeEl = document.querySelector(".letter__close")
-var letterEl = document.querySelector(".letter")
-
+var closeEl = document.querySelector(".letter__close");
+var letterEl = document.querySelector(".letter");
 
 function handleScroll(e) {
-    if (window.scrollY > 10) {
-      letterEl.classList.add("letter--shown")
-      letterEl.classList.remove("js-letter-open")
-    } else {
-      // letterEl.classList.remove("letter--shown")
-    }
+  if (window.scrollY > 10) {
+    letterEl.classList.add("letter--shown");
+    letterEl.classList.remove("js-letter-open");
+  } else {
+    // letterEl.classList.remove("letter--shown")
+  }
 }
 
+window.addEventListener("scroll", handleScroll);
 
-window.addEventListener('scroll', handleScroll);
+letterEl.addEventListener("click", function() {
+  letterEl.classList.add("letter--shown");
+});
 
-letterEl.addEventListener('click', function () {
-    letterEl.classList.add("letter--shown")  
-})
-
-
-
-closeEl.addEventListener('click', function (e) {  
-  console.log('close');
-  
+closeEl.addEventListener("click", function(e) {
   letterEl.classList.toggle("letter--shown");
   e.stopPropagation();
-})
+});
